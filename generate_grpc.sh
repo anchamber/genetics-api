bacf9eaaad8cb082d001fcf0f7543cd952c2ea93
Rene Schertel <reneschmitt@gmx.de>
2021-04-14 21:17:09 +0200

    add generation script for go

--- generate_grpc.sh
+++ generate_grpc.sh
@@ -1,2 +1,18 @@
 #! /bin/bash
-protoc --go_out=. --go-grpc_out=. proto/system.proto
+# protoc --go_out=. --go-grpc_out=. api/proto/ping.proto
+
+protoc \
+  --go_opt=module=github.com/anchamber/genetics-system \
+  --go_out=../genetics-system \
+  --go-grpc_opt=module=github.com/anchamber/genetics-system \
+  --go-grpc_out=../genetics-system \
+  --proto_path=proto \
+  system.proto
+
+protoc \
+  --go_opt=module=github.com/anchamber/genetics-api \
+  --go_out=. \
+  --go-grpc_opt=module=github.com/anchamber/genetics-api \
+  --go-grpc_out=. \
+  --proto_path=proto \
+  api.proto
